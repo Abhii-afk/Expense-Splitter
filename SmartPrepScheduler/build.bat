14d79919020876c386c5507644d7edd02e88968f
Abhinav Gaur <abhinavgaur095@gmail.com>
2026-04-09 20:21:21 +0530

    Delete SmartPrepScheduler directory

--- SmartPrepScheduler/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-g++ main.cpp scheduler.cpp graph.cpp -o SmartPrepScheduler.exe
-if %errorlevel% neq 0 (
-    echo Compilation failed.
-) else (
-    echo Compilation successful!
-    echo Run "SmartPrepScheduler.exe" to start the program.
-)
